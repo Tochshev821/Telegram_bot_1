20fd326bcd1b35530b4af184c8618da26d6aa9f6
George <gtoshev2@mail.ru>
2021-11-06 19:39:11 +0300

    My first telegram bot with crypto

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime
+import telebot
+from auth_data import token
+
+
+def get_data():
+    req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+    response = req.json()
+    #print(response)
+    sell_price = response["btc_usd"]["sell"]
+    #print(f"{datetime.now()}\nSell BTC price: {sell_price}")
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Hello friend")
+
+    @bot.message_handler(content_types=["text"])
+    def send_text(message):
+        if message.text.lower() == "price":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+                response = req.json()
+                sell_price = response["btc_usd"]["sell"]
+                bot.send_message(message.chat.id,
+                                 f"{datetime.now()}\nSell BTC price: {sell_price}")
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id,
+                                 f"woops, something goes wrong")
+        else:
+            bot.send_message(message.chat.id,"check the command")
+
+
+    bot.polling()
+
+
+
+def main():
+    #get_data()
+    telegram_bot(token)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
